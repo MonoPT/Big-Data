3842c5103599195247611cacba175c3860c8e60f
Rodrigo_Fernandes <55065897+MonoPT@users.noreply.github.com>
2025-11-30 16:32:36 +0000

    load data

--- notebooks/src/load_data.py
+++ notebooks/src/load_data.py
@@ -5,19 +5,19 @@ class LoadData:
     def __init__(self, path: str):
         self.spark = SparkSession.getActiveSession()
 
-        if self.spark is None:
+        if spark is None:
             # Nenhuma sessão ativa, criar uma nova
             self.spark = sparknlp.start()
         
-        self.application_categories = spark.read.csv(f'/content/data/application_categories.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.application_developers = spark.read.csv('/content/data/application_developers.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.application_genres = spark.read.csv('/content/data/application_genres.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.application_platforms = spark.read.csv('/content/data/application_platforms.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.application_publishers = spark.read.csv('/content/data/application_publishers.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.applications = spark.read.csv('/content/data/applications.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.categories = spark.read.csv('/content/data/categories.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.developers = spark.read.csv('/content/data/developers.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.genres = spark.read.csv('/content/data/genres.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.platforms = spark.read.csv('/content/data/platforms.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.publishers = spark.read.csv('/content/data/publishers.csv', header=True, inferSchema=True, multiLine=True, escape='"')
-        self.reviews = spark.read.csv('/content/data/reviews.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.application_categories = self.spark.read.csv(f'/content/data/application_categories.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.application_developers = self.spark.read.csv('/content/data/application_developers.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.application_genres = self.spark.read.csv('/content/data/application_genres.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.application_platforms = self.spark.read.csv('/content/data/application_platforms.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.application_publishers = self.spark.read.csv('/content/data/application_publishers.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.applications = self.spark.read.csv('/content/data/applications.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.categories = self.spark.read.csv('/content/data/categories.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.developers = self.spark.read.csv('/content/data/developers.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.genres = self.spark.read.csv('/content/data/genres.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.platforms = self.spark.read.csv('/content/data/platforms.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.publishers = self.spark.read.csv('/content/data/publishers.csv', header=True, inferSchema=True, multiLine=True, escape='"')
+        self.reviews = self.spark.read.csv('/content/data/reviews.csv', header=True, inferSchema=True, multiLine=True, escape='"')
